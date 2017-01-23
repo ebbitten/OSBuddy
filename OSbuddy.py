38706d2ca712d79e19217a7791b2f000351c6e7a
ahelitze <ebbitten@gmail.com>
2017-01-23 11:35:12 -0600

    Create a tag for finding differences between buying and selling

--- OSbuddy.py
+++ OSbuddy.py
@@ -23,7 +23,7 @@ def compareItemsCreateList (curList, pricesObj, comparisonKey, maxLen):
     '''
 
     :param curList: list that's manipulated in place
-    :param pricesObj: JSON object that has all of the price data
+    :param pricesObj: JSON object that has all of the price data for a snapshot
     :param comparisonKey: function that only accepts rsItem as a parameter (may assume priceObj)
     and should be used to compare
     :param maxLen: how long you want the list to be
@@ -84,6 +84,38 @@ def highAlchBest(itemsNamesLoc="ItemNames", pricesSummaryLoc = "ItemSummary1_23.
             print(str(key)+ " " + str(pricesObj[rsItem[0]]["name"]) + " rsItem profit " + str(rsItem[1]))
 
 
+def findMatchMaking(pricesSummaryLoc = "ItemSummary1_23.json", maxlen = 10):
+    # TODO: get some measure of liquidity in here
+    '''
+    :param pricesSummaryLoc: snapshot (currenlty, should have some history for sanity checks)
+    :param maxlen: how many items you want to consider
+    :return: nothing, currently just prints to terminal
+    '''
+    pricesObj = openJson(pricesSummaryLoc)
+    curList = []
 
-highAlchBest()
+    def compareMatchMaking(rsItem):
+        rsItemObj = pricesObj[rsItem]
+        averagePrice = rsItemObj["overall_average"]
+        buyingPrice = rsItemObj["buy_average"]
+        sellingPrice = rsItemObj["sell_average"]
+        if min(averagePrice,buyingPrice,sellingPrice) <= 0:
+            raise notTraded
+        #TODO remove this and replace with better logic for checking if it's actually traded as well as price prediction
+        # if averagePrice>(sellingPrice*1.2):
+        #     raise notTraded
+        metric = ((sellingPrice*.95 -1) - (buyingPrice*1.05 + 1))/averagePrice
+        return metric
 
+
+    compareItemsCreateList(curList, pricesObj, compareMatchMaking, maxlen)
+
+    for rsItem in curList:
+        print("Potential Profit for item", pricesObj[rsItem[0]]["name"], "Is", round(rsItem[1],5), "buy price",
+              pricesObj[rsItem[0]]["buy_average"], "sell price", pricesObj[rsItem[0]]["sell_average"],
+              "average", pricesObj[rsItem[0]]["overall_average"])
+
+
+
+# highAlchBest()
+findMatchMaking(maxlen=20)
